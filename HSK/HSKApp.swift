ffbff178f6a59637d2dc87f34adce297efff4018
David Johnson <david.b.johnson2015@gmail.com>
2024-05-15 21:56:12 -0500

    Initial Commit

---
+++ HSK/HSKApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSKApp.swift
+//  HSK
+//
+//  Created by David Johnson on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct HSKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
